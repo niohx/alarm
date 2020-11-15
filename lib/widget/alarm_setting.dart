48e32ab062fb2c2685a88dc9f830baf3b5dd092a
yohei <nioh@live.jp>
2020-11-15 23:16:48 +0900

    てｓｔ

--- lib/widget/alarm_setting.dart
+++ lib/widget/alarm_setting.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:myalarm/model/alarm.dart';
 import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AlarmRoot extends HookWidget {
-  final AlarmController alarm;
+  final StateNotifierProvider<AlarmController> alarm;
   AlarmRoot({Key key, @required this.alarm}) : super(key: key);
 
   Widget build(BuildContext context) {
+    final _alarm = useProvider(alarm);
+    final state = useProvider(alarm.state);
     DateTime _time;
-    print('from setting widget: alarm is set to ${alarm.state.time}');
     return Scaffold(
       appBar: AppBar(),
       body: Center(
@@ -18,19 +20,27 @@ class AlarmRoot extends HookWidget {
         children: [
           //Text('${alarm.state.mount}'),
           TimePickerSpinner(
-            time: DateTime.parse(alarm.state.time),
+            time: DateTime.parse(state.time),
             is24HourMode: true,
             spacing: 40,
             itemHeight: 80,
             isForce2Digits: false,
             onTimeChange: (time) {
-              _time = time;
+              print("displayed time is ${time}");
+              DateTime _now = DateTime.now();
+              if (time.isAfter(_now)) {
+                _time = time;
+              } else {
+                _time = time.add(Duration(days: 1));
+              }
+              ;
+              print("set time is ${_time}");
             },
           ),
           RaisedButton(
             onPressed: () {
               print('alarm will be set to ${_time}');
-              alarm.setAlarm(_time.toIso8601String());
+              _alarm.setAlarm(_time.toIso8601String());
               Navigator.pop(context);
             },
             child: Text('set'),
